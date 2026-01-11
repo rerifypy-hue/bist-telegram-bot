3fbe7215ca714736cc708f89e088174f585a1ff0
rerifypy-hue <rerifypy@gmail.com>
2026-01-11 14:52:49 +0300

    Create indicators.py

---
+++ indicators.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def rsi(series, period=14):
+    delta = series.diff()
+    gain = delta.where(delta > 0, 0).rolling(period).mean()
+    loss = -delta.where(delta < 0, 0).rolling(period).mean()
+    rs = gain / loss
+    return 100 - (100 / (1 + rs))
+
+def ema(series, period):
+    return series.ewm(span=period, adjust=False).mean()
