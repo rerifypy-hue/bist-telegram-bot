121e3ab70be135ac2bf967d6c0a7311c623f9aa5
rerifypy-hue <rerifypy@gmail.com>
2026-01-11 14:53:27 +0300

    Create telegram.py

---
+++ telegram.py
@@ -0,0 +1,10 @@
+import requests
+import os
+
+TOKEN = os.getenv("TG_TOKEN")
+CHAT_ID = os.getenv("TG_CHAT_ID")
+
+def send(msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": msg}
+    requests.post(url, json=payload)
